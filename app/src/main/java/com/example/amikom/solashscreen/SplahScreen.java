15ad5da5ea4b8532d015465bee1b038cec4c4eb9
M Bagus Nurfaiz <bagusnurfais@gmail.com>
2018-10-05 08:47:55 +0700

    iniriate project

---
+++ app/src/main/java/com/example/amikom/solashscreen/SplahScreen.java
@@ -0,0 +1,13 @@
+package com.example.amikom.solashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplahScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splah_screen);
+    }
+}
